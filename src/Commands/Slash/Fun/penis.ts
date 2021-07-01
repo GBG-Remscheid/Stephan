f4e0cf467c0057eb12190e567fa9a04508af45ea
Maximilian Hofmann <maximilian.hofmann.2@gmx.de>
2021-07-01 21:34:36 +0200

    ✨  smol improvements

--- src/Commands/Slash/Fun/penis.ts
+++ src/Commands/Slash/Fun/penis.ts
@@ -1,21 +1,8 @@
-import { Discord, Group, Guild, Option, Slash } from "@typeit/discord";
+import { Option, Slash } from "@typeit/discord";
 import { CommandInteraction, User } from "discord.js";
-
-
-@Discord()
-@Guild("768975702187704360")
-@Group(
-    "genitals",
-    "how long/deep are your genitals",
-    {
-        penis: "how long is your dick 8===D",
-        mumu: "how deep is your mumu"
-    }
-)
 
 export abstract class Genitals {
     @Slash("penis")
-    @Group("penis")
     penis(
         @Option("member", { description: "optional member" })
         member: User,
@@ -27,7 +14,6 @@ export abstract class Genitals {
     }
 
     @Slash("mumu")
-    @Group("mumu")
     mumu(
         @Option("member", { description: "optional member" })
         member: User,
