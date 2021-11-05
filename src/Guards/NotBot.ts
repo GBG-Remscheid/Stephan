3845a41cadbd581107834a214924a0fc350ac591
Maximilian Hofmann <maximilian.hofmann.2@gmx.de>
2021-11-05 23:27:53 +0100

    ✨  updated NotBot guard

--- src/Guards/NotBot.ts
+++ src/Guards/NotBot.ts
@@ -1,7 +1,20 @@
-import { GuardFunction, ArgsOf } from "@typeit/discord";
+import { CommandInteraction, MessageReaction, VoiceState } from "discord.js";
+import { GuardFunction, ArgsOf } from "discordx";
 
-export const NotBot: GuardFunction<ArgsOf<"message">> = async ([message], client, next) => {
-  if (client.user.id !== message.author.id) {
+export const NotBot: GuardFunction<
+  | ArgsOf<"messageCreate">
+  | CommandInteraction
+> = async (arg, client, next) => {
+  const argObj = arg instanceof Array ? arg[0] : arg;
+  const user =
+    argObj instanceof CommandInteraction
+      ? argObj.user
+      : argObj instanceof MessageReaction
+        ? argObj.message.author
+        : argObj instanceof VoiceState
+          ? argObj.member.user
+          : argObj.author;
+  if (!user?.bot) {
     await next();
   }
 };
