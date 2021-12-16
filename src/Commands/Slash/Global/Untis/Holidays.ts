abbbca09e9b95d2f532e406a0fe55a7ffd4bb1d9
Maximilian Hofmann <maximilian.hofmann.2@gmx.de>
2021-12-16 21:04:15 +0100

    📆  added Holidays cmd

---
+++ src/Commands/Slash/Global/Untis/Holidays.ts
@@ -0,0 +1,41 @@
+import { CommandInteraction, MessageEmbed } from "discord.js";
+import { Discord, Slash, SlashGroup } from "discordx";
+import Webuntis from 'webuntis';
+import { config } from 'dotenv';
+import moment from "moment";
+
+config();
+
+const untis = new Webuntis(process.env.SCHOOL ?? "", process.env.USERNAME ?? "", process.env.PASSWORD ?? "", process.env.BASEURL ?? "");
+
+@Discord()
+@SlashGroup("untis")
+export default class Holidays {
+    @Slash("ferien")
+    async holidays(
+        interaction: CommandInteraction
+    ) {
+        untis.login()
+            .then(async () => {
+                let daysLeft: string = "";
+                let date: Date = new Date();
+                const startDate = (await untis.getHolidays()).at(1)?.startDate;
+
+                const dateString = startDate?.toString();
+                const year = dateString?.substring(0, 4);
+                const month = dateString?.substring(4, 6);
+                const day = dateString?.substring(6, 8);
+
+                if (year && month && day) {
+                    date = new Date(parseInt(year), parseInt(month) - 1, parseInt(day));
+                }
+                daysLeft = moment.duration(date.valueOf() - new Date().valueOf()).asDays().toFixed(0);
+
+                const embed = new MessageEmbed()
+                    .setColor("RANDOM")
+                    .setDescription(`Die Ferien beginnen in ${daysLeft} Tagen`);
+
+                interaction.reply({ embeds: [embed] });
+            })
+    }
+}
