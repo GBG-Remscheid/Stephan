e8d8f41b3310f7460d9cdd0f9cc951257eccc184
Maximilian Hofmann <maximilian.hofmann.2@gmx.de>
2022-02-07 01:06:36 +0100

    <3

--- src/Commands/Slash/Global/Owner/Mel.ts
+++ src/Commands/Slash/Global/Owner/Mel.ts
@@ -1,5 +1,6 @@
 import { Discord, Permission, Slash } from "discordx";
 import { CommandInteraction } from "discord.js";
+import moment from "moment";
 
 @Discord()
 @Permission({ id: "374590194034409472", permission: true, type: "USER" })
@@ -7,9 +8,27 @@ import { CommandInteraction } from "discord.js";
 export abstract class Mel {
     @Slash("mel")
     mel(interaction: CommandInteraction): Promise<void> {
+        // days until 24.02.2022
+        const daysLeft = moment("2022-02-24").diff(moment(), "days");
+        const catGIFs = [
+            "https://c.tenor.com/JRFXgb1TBx8AAAAC/cat-cats.gif",
+            "https://i.imgur.com/r8SbhWp.gif",
+            "https://c.tenor.com/RiHiEx9u-r0AAAAC/cat-hug.gif",
+            "https://c.tenor.com/rgJqc4unaUYAAAAC/love-you.gif",
+            "https://i.imgur.com/gQ2m7cl.gif",
+            "https://c.tenor.com/qLL-xoCauUYAAAAC/peachcat-kiss.gif",
+            "https://c.tenor.com/5iSOvaJG7MQAAAAC/lady-and-the-tramp.gif",
+            "https://c.tenor.com/whAhQj8irG8AAAAC/lady-and-the-tramp-dogs.gif",
+        ];
+
+        const randomGIF = catGIFs[Math.floor(Math.random() * catGIFs.length)];
+
         return interaction.reply({
-            content: "Mel & maggsii sind am tollsten <3",
+            content: `Mel & maggsii sind am tollsten <3 \nNoch \`69\` - ${
+                69 - daysLeft
+            } Tage bis wir uns sehen. <33333`,
             ephemeral: true,
+            files: [randomGIF],
         });
     }
 }
