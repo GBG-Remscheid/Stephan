e0121fac0a03e987ab31e65d6f522c422f91d6e2
Maximilian Hofmann <61088478+hofmmaxi@users.noreply.github.com>
2022-08-02 06:39:27 +0000

    ⚠  fix lint errors

--- src/Events/Guilds/maxii's dev space/GuildMemberAdd.ts
+++ src/Events/Guilds/maxii's dev space/GuildMemberAdd.ts
@@ -1,5 +1,5 @@
-import { ChannelType, PermissionFlagsBits } from "discord.js";
 import { type ArgsOf, Discord, Guild, On } from "discordx";
+import { ChannelType, PermissionFlagsBits } from "discord.js";
 
 @Discord()
 @Guild("1003607488928686121")
@@ -7,29 +7,31 @@ export abstract class GuildMemberAdd {
     @On("guildMemberAdd")
     async handler([member]: ArgsOf<"guildMemberAdd">): Promise<void> {
         const { guild } = member;
-        const channel = guild.channels.cache.find(channel =>
+        const statChannel = guild.channels.cache.find(channel =>
             channel.name.includes(":bar_chart: members:")
         );
-        if (channel?.type !== ChannelType.GuildVoice) {
+        if (statChannel?.type !== ChannelType.GuildVoice) {
             console.warn(
                 `There is no voice channel for displaying the memebers size...
             Trying to create one...`
             );
         }
 
-        if (!channel) {
+        if (!statChannel) {
             const memberCount = (await guild.members.fetch()).size;
             await guild.channels.create({
                 name: `:bar_chart: Members: ${memberCount}`,
-                type: ChannelType.GuildVoice,
-                position: 1,
                 permissionOverwrites: [
                     { deny: [PermissionFlagsBits.Connect], id: guild.id },
                 ],
+                position: 1,
+                type: ChannelType.GuildVoice,
             });
         } else {
             const memberCount = (await guild.members.fetch()).size;
-            await channel.edit({ name: `:bar_chart: Members: ${memberCount}` });
+            await statChannel.edit({
+                name: `:bar_chart: Members: ${memberCount}`,
+            });
         }
     }
 }
